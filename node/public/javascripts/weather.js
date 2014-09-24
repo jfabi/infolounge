a376f0f454d06b9ff62d91987749b145dd6fc059
jfabi <joshuajfabian@gmail.com>
2014-09-24 01:44:48 -0400

    320v2 - 3-day weather
    Adds 3-day weather forecast and icons

--- node/public/javascripts/weather.js
+++ node/public/javascripts/weather.js
@@ -4,12 +4,23 @@ function getWeather() {
         unit: 'f',
         success: function(weather) {
             html = '<div class="row">';
-            html += '<div class="large-4 columns"><img src="images/weather/' + weather.code + '.png"></div>';
-            html += '<div class="large-8 columns">';
-            html += '<span class="weather-feel">Now</span><br/>';
-            html += '<span class="weather-temp">' + ((weather.temp-32)*(5/9)).toFixed() + '&deg;C / ' + weather.temp + '&deg;F</span><br/>';
-            html += '<span class="weather-feel">Feel ' + ((weather.wind.chill-32)*(5/9)).toFixed() + '&deg;C / ' + weather.wind.chill + '&deg;F</span><br/>';
-            html += '</div></div>';
+            html += '<div class="large-4 columns">' + Current + '</div>';
+            html += '<div class="large-4 columns">' + weather.forecast[0].day + '</div>';
+            html += '<div class="large-4 columns">' + weather.forecast[1].day + '</div>';
+            html += '<div class="large-4 columns">' + weather.forecast[2].day + '</div>';
+            html += '</div>';
+            
+            html += '<div class="row">';
+            html += '<div class="large-4 columns"><img src="images/weather/' + weather.code + '.png"><br/>';
+            html += '<span class="weather-temp">' + ((weather.temp-32)*(5/9)).toFixed() + '&deg;C (' + weather.temp + '&deg;F)</span>';
+            html += '<span class="weather-feel">Feel ' + ((weather.wind.chill-32)*(5/9)).toFixed() + '&deg;C / ' + weather.wind.chill + '&deg;F</span></div>';
+            html += '<div class="large-4 columns"><img src="images/weather/' + weather.forecast[0].code + '.png"><br/>';
+            html += '<span class="weather-temp">' + weather.forecast[0].alt.high + '&deg;C / ' + weather.forecast[0].alt.low + '&deg;C</span></div>';
+            html += '<div class="large-4 columns"><img src="images/weather/' + weather.forecast[1].code + '.png"><br/>';
+            html += '<span class="weather-temp">' + weather.forecast[1].alt.high + '&deg;C / ' + weather.forecast[1].alt.low + '&deg;C</span></div>';
+            html += '<div class="large-4 columns"><img src="images/weather/' + weather.forecast[2].code + '.png"><br/>';
+            html += '<span class="weather-temp">' + weather.forecast[2].alt.high + '&deg;C / ' + weather.forecast[2].alt.low + '&deg;C</span></div>';
+            html += '</div>';
 
             $("#weather").html(html);
         },
